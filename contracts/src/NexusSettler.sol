57ddaf2d105f78ea356ec7be813788efc2497b90
qedk <1994constant@gmail.com>
2024-11-28 23:02:17 +0530

    feat: add nexus settler impl

--- contracts/src/NexusSettler.sol
+++ contracts/src/NexusSettler.sol
@@ -7,27 +7,24 @@ import "./interfaces/IStructs.sol";
 
 contract NexusSettler {
     /// @dev chainId => rollup contract address
-    mapping(uint32 => address) public rollups;
-    /// @dev chainId => order hash
-    mapping(uint32 => bytes32) public orders;
+    mapping(uint256 => address) public rollups;
+    /// @dev chainId => order root
+    mapping(uint256 => bytes32) public orderRoots;
+    /// @dev chainId => fill root
+    mapping(uint256 => bytes32) public fillRoots;
 
-    function createRollup(uint32 chainId, address rollup) external {
+    function createRollup(uint256 chainId, address rollup) external {
         require(rollups[chainId] == address(0), RollupAlreadyExists(chainId));
         rollups[chainId] = rollup;
     }
 
-    function createOrder(OnchainCrossChainOrder calldata order) external {
-        ERC20CrossChainOrder memory inner = abi.decode(order.orderData, (ERC20CrossChainOrder));
-        require(order.fillDeadline > block.timestamp, OrderExpired(keccak256(abi.encode(order))));
-        require(rollups[inner.destination] != address(0), UnauthorizedRollup(inner.destination));
-        require(rollups[inner.source] != address(0), UnauthorizedRollup(inner.source));
-        bytes32 orderHash = keccak256(abi.encode(order));
-        require(orders[inner.source] == bytes32(0), OrderPendingOrFulfilled(orderHash));
-        orders[inner.source] = orderHash;
-        emit OrderSent(orderHash);
+    function updateRollupOrderRoot(uint256 chainId, bytes32 root) external {
+        require(rollups[chainId] != address(0), UnauthorizedRollup(chainId));
+        orderRoots[chainId] = root;
     }
 
-    function release() external {
-        // Implementation omitted
+    function updateRollupFillRoot(uint256 chainId, bytes32 root) external {
+        require(rollups[chainId] != address(0), UnauthorizedRollup(chainId));
+        fillRoots[chainId] = root;
     }
 }
