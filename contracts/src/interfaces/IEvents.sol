cb7064790a011946c68b47e0724d575eec78c98b
qedk <1994constant@gmail.com>
2025-03-11 01:08:41 -0700

    upd: update events

--- contracts/src/interfaces/IEvents.sol
+++ contracts/src/interfaces/IEvents.sol
@@ -1,5 +1,15 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.28;
 
+import {ResolvedCrossChainOrder} from "./IStructs.sol";
+
+event Evicted(address solver);
+/// @notice Signals that an order has been opened
+/// @param orderId a unique order identifier within this settlement system
+/// @param resolvedOrder resolved order that would be returned by resolve if called instead of Open
+event Open(bytes32 indexed orderId, ResolvedCrossChainOrder resolvedOrder);
 event OrderSent(bytes32 orderHash);
 event OrderFilled(bytes32 orderHash);
+event Slashed(address solver, uint256 amount);
+event Unbonded(address solver);
+event UnbondingStarted(address solver, uint256 exitTimestamp);
