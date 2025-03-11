3c3de33bbecfac0accfde5d3b3fa06f1af6bba17
qedk <1994constant@gmail.com>
2025-03-11 01:08:23 -0700

    upd: update errors

--- contracts/src/interfaces/IErrors.sol
+++ contracts/src/interfaces/IErrors.sol
@@ -1,11 +1,20 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.28;
 
-error UnauthorizedRollup(uint256 chainId);
+error InsufficientBond(uint256 amount);
+error InvalidDeadline(uint256 deadline);
 error InvalidOrder(bytes32 orderHash);
+error InvalidRollupContract(address rollup);
+error InvalidSignature();
+error InvalidSigner();
+error InvalidStateRoot(uint256 chainId, bytes32 stateRoot);
+error NonExistentStateRoot(uint256 chainId, bytes32 stateRoot);
+error OnlyController(address sender);
 error OrderExpired(uint256 deadline);
 error OrderPendingOrFulfilled(bytes32 orderHash);
 error OrderAlreadyFulfilled(bytes32 orderHash);
 error OrderNotFulfilled(bytes32 orderHash);
 error RollupAlreadyExists(uint256 chainId);
-error InvalidDeadline(uint256 deadline);
+error SignatureExpired(uint256 timestamp);
+error UnauthorizedRollup(uint256 chainId);
+error ZeroAddress();
