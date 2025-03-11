02449f044ed2c6bdb8058a7a7395df52bf648c92
qedk <1994constant@gmail.com>
2025-03-11 01:10:25 -0700

    feat: upd nexus settler

--- contracts/src/NexusSettler.sol
+++ contracts/src/NexusSettler.sol
@@ -1,30 +1,39 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.28;
 
+import {Address} from "openzeppelin-contracts/contracts/utils/Address.sol";
+import {Ownable2Step, Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable2Step.sol";
+import {INexusSettler} from "./interfaces/INexusSettler.sol";
 import "./interfaces/IErrors.sol";
 import "./interfaces/IEvents.sol";
 import "./interfaces/IStructs.sol";
 
-contract NexusSettler {
+contract NexusSettler is Ownable2Step, INexusSettler {
+    using Address for address;
     /// @dev chainId => rollup contract address
-    mapping(uint256 => address) public rollups;
-    /// @dev chainId => order root
-    mapping(uint256 => bytes32) public orderRoots;
-    /// @dev chainId => fill root
-    mapping(uint256 => bytes32) public fillRoots;
+    mapping(uint256 => Rollup) public rollups;
+    /// @dev chainId => (state root => timestamp)
+    mapping(uint256 => mapping (bytes32 => uint256)) public stateRoots;
 
-    function createRollup(uint256 chainId, address rollup) external {
-        require(rollups[chainId] == address(0), RollupAlreadyExists(chainId));
-        rollups[chainId] = rollup;
+    constructor(address governance) Ownable(governance) {}
+
+    function createRollup(uint256 chainId, address rollupContract, address settlerContract, bytes4 stateRootFn) external onlyOwner {
+        require(rollups[chainId].rollupContract == address(0), RollupAlreadyExists(chainId));
+        require(rollupContract.code.length != 0, InvalidRollupContract(rollupContract));
+        require(settlerContract != address(0), ZeroAddress());
+        bytes memory result = rollupContract.functionStaticCall(abi.encodeWithSelector(stateRootFn));
+        rollups[chainId] = Rollup(rollupContract, settlerContract, chainId, stateRootFn);
+        bytes32 stateRoot = abi.decode(result, (bytes32));
+        require(stateRoot != bytes32(0), InvalidStateRoot(chainId, stateRoot));
+        stateRoots[chainId][stateRoot] = block.timestamp;
     }
 
-    function updateRollupOrderRoot(uint256 chainId, bytes32 root) external {
-        require(rollups[chainId] != address(0), UnauthorizedRollup(chainId));
-        orderRoots[chainId] = root;
-    }
-
-    function updateRollupFillRoot(uint256 chainId, bytes32 root) external {
-        require(rollups[chainId] != address(0), UnauthorizedRollup(chainId));
-        fillRoots[chainId] = root;
+    function updateRollupRoot(uint256 chainId) external {
+        Rollup memory rollup = rollups[chainId];
+        require(rollup.rollupContract != address(0), UnauthorizedRollup(chainId));
+        bytes memory result = rollup.rollupContract.functionStaticCall(abi.encodeWithSelector(rollup.stateRootFn));
+        bytes32 stateRoot = abi.decode(result, (bytes32));
+        require(stateRoot != bytes32(0), InvalidStateRoot(chainId, stateRoot));
+        stateRoots[chainId][stateRoot] = block.timestamp;
     }
 }
